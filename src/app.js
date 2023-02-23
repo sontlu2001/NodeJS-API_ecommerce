cb2eb29863da7683fd4a48fcfb663f02a47e84d7
DoSon <sonnvc2001@gmail.com>
2023-02-23 17:15:11 +0700

    FEAT: Add connect MongoDB to Node.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-const compression = require('compression');
-const express = require('express');
-const { default: helmet } = require('helmet');
-const morgan = require('morgan');
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
 const app = express();
 
 // init middlewares
-app.use(morgan("dev"))
-app.use(helmet())
-app.use(compression())
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // init DB
-
+require("./dbs/init.mongodb");
+const { checkOverLoad } = require("./helpers/check.connect.js");
+checkOverLoad();
 // init routers
-app.get("/",(req,res,next)=>{
-    return res.status(200).json({
-        message:"Welcome project"
-    })
-})
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome project",
+  });
+});
 //handling error
 
-module.exports = app
+module.exports = app;

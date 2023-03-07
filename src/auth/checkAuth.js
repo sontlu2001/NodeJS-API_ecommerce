353c6dd8631236dc43d83889e6dacc13a5fdedb5
DoSon <sonnvc2001@gmail.com>
2023-03-07 22:07:07 +0700

    FEAT: Custom dynamic middleware for Apikey and permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const { findById } = require("../services/apikey.service")
+
+const HEADER={
+    API_KEY:'x-api-key',
+    AUTHORIZATION:'authorization'
+}
+const apiKey = async(req,res,next)=>{
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString()
+        console.log(key);
+        if(!key) {
+            return res.status(403).json({
+                message:"Forbidden Error"
+            })
+        }
+        // check object key
+        const objKey = await findById(key)
+        if(!objKey){
+            return res.status(403).json({
+                message:"Forbidden Error2"
+            })
+        }
+        req.objKey = objKey
+        return next()
+    } catch (error) {
+        console.log("middleware::checkAuth",error);
+    }
+}
+const permission = (permission)=>{
+return (req,res,next)=>{
+    if(!req.objKey.permissions){
+        return res.status(403).json({
+            message:"permission denied"
+        })
+    }
+    console.log("");
+    const validPermission = req.objKey.permissions.includes(permission)
+    if(!validPermission){
+        return res.status(403).json({
+            message:"permission denied"
+        })
+    }
+    return next();
+}
+}
+
+module.exports={
+    apiKey,
+    permission
+}

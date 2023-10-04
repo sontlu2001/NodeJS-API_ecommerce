dbb6d24e0ba4f25a6618e3d1b0e46d52ea69ad9e
DoSon <sonnvc2001@gmail.com>
2023-10-04 21:37:23 +0700

    Inventories Service & Model

--- README.md
+++ README.md
@@ -8,26 +8,6 @@ Install:
 ## Section 3:Connect MongoDB to Node.js
 Install:
 - mongoose
-Node:
-### Có nên disConnect() liên tục hay không ?
-- Không cần thiết phải đóng kết nối liên tục một cách thủ công. Vì trong mongoes sử dụng pool quản lý các CSDL tự động mở-đóng các kết nối khi cần.
-- Nếu ứng dụng cần thiết phải đóng tất cả các kết nối thủ công đang hoạt động để đảm bảo dữ liệu không bị mất.
-<!-- 
-process.on('SIGINT',()=>{
-    server.close(()=>console.log(`Exit Server Express`))
-})
- -->
-### PoolSize là gì ? Vì sao lại quan trọng ?
-- Trong mongoose nhóm kết nối là tập hợp các kết nối tới CSDL mà có thể tái sử dụng được duy trì bởi Database (trong MySQL và oracle cũng có điều này).
-- Cơ chế hoạt động của nó như sau: 
-    - Khi một connection (một kết nối) được tạo, nó sẽ được đưa vào pool và sử dụng lại cho các yêu cầu kết nối tiếp theo và chỉ bị đóng khi hết thời gian timeout.Ví dụ, max pool size = 10 (số lượng tối đa connection trong pool là 10).
-    - Bây giờ user kết nối tới database (truy vấn database), hệ thống sẽ kiểm tra trong connection pool có kết nối nào đang rảnh không?
-    - Trường hợp chưa có kết nối nào trong connection pool hoặc tất cả các kết nối đều bận (đang được sử dụng bởi user khác) và số lượng connection trong connection < 10 thì sẽ tạo một connection mới tới database để kết nối tới database đồng thời kết nối đó sẽ được đưa vào connection pool.
-    - Trường hợp tất cả các kết nối đang bận và số lượng connection trong connection pool = 10 thì người dùng phải đợi cho các user dùng xong để được dùng.
-    Sau khi một kết nối được tạo và sử dụng xong nó sẽ không đóng lại mà sẽ duy trì trong connection pool để dùng lại cho lần sau và chỉ thực sự bị đóng khi hết thời gian timeout (lâu quá không dùng đến nữa)
-- Việc sử dụng PoolSize giúp cải thiện hiệu suất, tăng khả năng mở rộng của ứng dụng.
-### Nếu vượt quá số lượng kết nối PoolSize ?
-- Mongoose sẽ không vượt quá kích thước PoolSize thay vào đó connection sẽ được đưa vào hàng đợi chờ các yêu cầu khác được xử lý xong thì mới được xử lý.
 ## Section 4
 ## Section 5: API Sign-up Shop
 Install: 
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -2,7 +2,8 @@
 
 const { BadRequestError } = require('../core/error.response')
 const { product, electronic, clothing, furniture } = require('../models/product.model')
-const { 
+const { insertInventory } = require('../models/repositories/inventory.repo')
+const {
     findAllDraftsForShop,
     publishProductByShop,
     findAllPublicForShop,
@@ -17,12 +18,12 @@ const { removeUndefinedObject, updateNestedObjectParser } = require('../utils')
 // define Factory class to create product
 class ProductFactory {
     static productRegister = {}
-    static registerProductType(type,classRef){
+    static registerProductType(type, classRef) {
         ProductFactory.productRegister[type] = classRef
     }
-    static async createProduct(type,payload){
+    static async createProduct(type, payload) {
         const productClass = ProductFactory.productRegister[type]
-        if(!productClass) throw new BadRequestError(`Invalid type ${type}`)
+        if (!productClass) throw new BadRequestError(`Invalid type ${type}`)
         return new productClass(payload).createProduct()
     }
     static async updateProduct(type, productId, payload) {
@@ -31,33 +32,34 @@ class ProductFactory {
         return new productClass(payload).updateProduct(productId)
     }
     // PUT
-    static async publishProductByShop({product_shop, product_id}){
-        return await publishProductByShop({product_shop, product_id})
+    static async publishProductByShop({ product_shop, product_id }) {
+        return await publishProductByShop({ product_shop, product_id })
     }
-    static async unPublishProductByShop({product_shop, product_id}){
-        return await unPublishProductByShop({product_shop, product_id})
+    static async unPublishProductByShop({ product_shop, product_id }) {
+        return await unPublishProductByShop({ product_shop, product_id })
     }
     // END PUT
 
     // Query
-    static async findAllDraftsForShop({product_shop, limit = 50, skip = 0}){
-        const query = {product_shop, isDraft:true}
-        return await findAllDraftsForShop({query,limit,skip})
+    static async findAllDraftsForShop({ product_shop, limit = 50, skip = 0 }) {
+        const query = { product_shop, isDraft: true }
+        return await findAllDraftsForShop({ query, limit, skip })
     }
-    static async findAllPublicForShop({product_shop, limit = 50, skip = 0}){
-        const query = {product_shop, isPublished:true}
-        return await findAllPublicForShop({query,limit,skip})
+    static async findAllPublicForShop({ product_shop, limit = 50, skip = 0 }) {
+        const query = { product_shop, isPublished: true }
+        return await findAllPublicForShop({ query, limit, skip })
     }
-    static async searchProduct({keySearch}){
-        return await searchProduct({keySearch})
+    static async searchProduct({ keySearch }) {
+        return await searchProduct({ keySearch })
     }
     // detail
-    static async findProduct({product_id}){
-        return await findProduct({product_id, unSelect:['__v']})
+    static async findProduct({ product_id }) {
+        return await findProduct({ product_id, unSelect: ['__v'] })
     }
-    static async findAllProduct({limit = 50, sort = 'ctime', page = 1,filter={isPublished:true} }){
-        return await findAllProduct({limit, sort, page,filter,
-            select:['product_name','product_thumb','product_price','product_ratingsAverage']
+    static async findAllProduct({ limit = 50, sort = 'ctime', page = 1, filter = { isPublished: true } }) {
+        return await findAllProduct({
+            limit, sort, page, filter,
+            select: ['product_name', 'product_thumb', 'product_price', 'product_ratingsAverage']
         })
     }
 }
@@ -76,78 +78,86 @@ class Product {
         this.product_attributes = product_attributes
     }
     // create new product
-    async createProduct(productId){
-        return await product.create({
-            ...this,
-            _id:productId,
-        })
+    async createProduct(productId) {
+        const newProduct = await product.create({ ...this, _id: productId, })
+        if (newProduct) {
+            // add product_stock in inventory collection
+            await insertInventory({
+                productId: newProduct._id,
+                shopId: this.product_shop,
+                stock: this.product_quantity
+            })
+        }
+        return newProduct
     }
     // update product
-    async updateProduct(productId,payload){
-        return await updateProductById({productId,payload,model: product})
+    async updateProduct(productId, payload) {
+        return await updateProductById({ productId, payload, model: product })
     }
 }
 
 // Define sub-class for different product types Clothing
-class Clothing extends Product{
-    async createProduct(){
+class Clothing extends Product {
+    async createProduct() {
         const newClothing = await clothing.create({
             ... this.product_attributes,
             product_shop: this.product_shop
         })
-        if(!newClothing) throw new BadRequestError('create new Clothing error')
+        if (!newClothing) throw new BadRequestError('create new Clothing error')
         const newProduct = await super.createProduct(newClothing._id)
-        if(!newProduct) throw new BadRequestError('create new Product error')
+        if (!newProduct) throw new BadRequestError('create new Product error')
         return newProduct;
     }
-    async updateProduct(productId){
+    async updateProduct(productId) {
         // 1. remove attr has null or undefined
-        const objectParams = removeUndefinedObject(this) 
+        const objectParams = removeUndefinedObject(this)
         // 2. check xem update ở đâu
-        if(objectParams.product_attributes){
+        if (objectParams.product_attributes) {
             // update clothing                        
             await updateProductById({
-                productId, 
-                payload: updateNestedObjectParser(objectParams.product_attributes), 
+                productId,
+                payload: updateNestedObjectParser(objectParams.product_attributes),
                 model: clothing
             })
         }
         const updateProduct = await super.updateProduct(productId, updateNestedObjectParser(objectParams))
-        if(!updateProduct) throw new BadRequestError("Can't found product")
-        
+        if (!updateProduct) throw new BadRequestError("Can't found product")
+
         return updateProduct
     }
 }
 
 // Define sub-class for different product types Electronic
-class Electronic extends Product{
-    async createProduct(){
+class Electronic extends Product {
+    async createProduct() {
         const newElectronic = await electronic.create({
             ...this.product_attributes,
             product_shop: this.product_shop
         })
-        if(!newElectronic) throw new BadRequestError('create new Electronic error')
+        if (!newElectronic) throw new BadRequestError('create new Electronic error')
         const newProduct = await super.createProduct(newElectronic._id)
-        if(!newProduct) throw new BadRequestError('create new Product error')
+        if (!newProduct) throw new BadRequestError('create new Product error')
         return newProduct;
     }
 }
 // Define sub-class for different product types Furniture
-class Furniture extends Product{
-    async createProduct(){
+class Furniture extends Product {
+    async createProduct() {
         const newFurniture = await furniture.create({
             ...this.product_attributes,
             product_shop: this.product_shop
         })
-        if(!newFurniture) throw new BadRequestError('create new Furniture error')
+        if (!newFurniture) throw new BadRequestError('create new Furniture error')
         const newProduct = await super.createProduct(newFurniture._id)
-        if(!newProduct) throw new BadRequestError('create new Product error')
+        if (!newProduct) throw new BadRequestError('create new Product error')
         return newProduct;
     }
 }
 // register product types
-ProductFactory.registerProductType('Electronic',Electronic)
-ProductFactory.registerProductType('Clothing',Clothing)
-ProductFactory.registerProductType('Furniture',Furniture)
- 
+ProductFactory.registerProductType('Electronic', Electronic)
+ProductFactory.registerProductType('Clothing', Clothing)
+ProductFactory.registerProductType('Furniture', Furniture)
+
 module.exports = ProductFactory
+
+

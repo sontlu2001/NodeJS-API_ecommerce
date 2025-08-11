4cd91e956cab0a67ce5f240fad2300aea696c1b6
Do Son <sonnvc2001@gmail.com>
2025-08-11 21:44:36 +0700

    feat(logging): integrate winston for structured logging and error handling

--- ecommerce-backend/src/app.js
+++ ecommerce-backend/src/app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const app = express();
-
+const {v4: uuidv4} = require("uuid");
+const myLogger = require("./loggers/mylogger.log.js");
 
 // init middlewares
 app.use(morgan("dev"));
@@ -16,20 +17,38 @@ app.use(
     extended: true,
   })
 );
+app.use((req,res,next) => {
+  const requestId = req.headers["x-request-id"] || uuidv4();
+  req.requestId = requestId ? requestId : uuidv4();
+  myLogger.log('input params', [
+    req.path,
+    {requestId: req.requestId},
+    req.method === "POST" ? req.body : req.query,
+  ])
+
+  next();
+})
+
+
 // init DB
 require("./dbs/init.mongodb");
 const { checkOverLoad } = require("./helpers/check.connect.js");
 // checkOverLoad();
+
 // init routers
 app.use("", require("./routes"));
+
 //handling error
 app.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
+
 app.use((error, req, res, next) => {
-  const statusCode = error.status || 500;  
+  const statusCode = error.status || 500;
+  const resMessage = `${statusCode}:::${Date.now() - error.now}ms`
+
   const response = {
     status: "error",
     code: statusCode,
@@ -39,7 +58,11 @@ app.use((error, req, res, next) => {
   // if (process.env.NODE_ENV === 'dev'){
   //   response.stack = error.stack
   // }
-
+  myLogger.error(resMessage, [
+    req.path,
+    {requestId: req.requestId},
+    {message: error.message},
+  ]);
   return res.status(statusCode).json(response);
 });
 module.exports = app;
